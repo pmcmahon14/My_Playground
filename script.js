973b5aee0f57f80083794ac4c2620dc6ef6039cf
Patrick McMahon <pmcmahon14@gmail.com>
2016-11-21 13:11:03 -0800

    memory_match v1 - Patrick McMahon

--- script.js
+++ script.js
@@ -1,5 +1,79 @@
-
 /**
  * Created by Patrick on 11/16/2016.
  */
+first_card_clicked = null;
+second_card_clicked = null;
+total_number_matches = 2;
+match_counter = 0;
+var games_played = 0;
+var attempts = 0;
+var accuracy = 0;
 
+$(document).ready(function() {
+    console.log("hello")
+    $('.back').click(clickon);
+    $('.reset').click(reset_stats);
+});
+
+/*this checks for match*/
+
+function clickon() {
+    $(this).hide();
+    if (first_card_clicked == null) {
+        first_card_clicked = this;
+        return;
+        console.log("first_card_clicked");
+    } else {
+        second_card_clicked = this;
+        attempts++;
+        $('.attempts > .value').text(attempts);
+        if (($(first_card_clicked).find('img').attr('src')) === ($(second_card_clicked).find('img').attr('src'))) {
+            console.log("match");
+            match_counter++;
+            games_played++;
+            $(".matching > .value").text(games_played);
+            var accuracy = (games_played / attempts).toFixed(2) * 100 + ('%');
+            $('.accuracy > .value').text(accuracy);
+            /*first_card_clicked = null;
+             second_card_clicked = null;*/
+            if (match_counter === total_number_matches) {
+                console.log("You won!");
+            } else {
+                console.log("Good job!");
+            }
+        } else {
+            setTimeout(timer, 2000);
+            console.log("restart");
+            return;
+            timer()
+        }
+    }
+}
+
+function reset_game (){
+
+}
+
+
+
+
+function timer() {
+    $(first_card_clicked).find('.back').show();
+    $(second_card_clicked).find('.front').show();
+    first_card_clicked = null;
+    second_card_clicked = null;
+}
+
+function reset_stats(){
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    games_played++;
+    $('.back').show();
+    $('.games-played .value').text(games_played);
+    $('.attempts  .value').text(attempts);
+    $(".matching > .value").text(matches);
+    $('.accuracy > .value').text(accuracy);
+    console.log('reset stats');
+    return;
+}
